385db9c9f0d8a3991a492e1969f3b489f5ca15ba
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-06-12 16:05:50 +0530

    modify pdf viewer position

--- frontend/src/pages/PDFViewer.js
+++ frontend/src/pages/PDFViewer.js
@@ -3,6 +3,20 @@ import { useParams } from "react-router-dom";
 import { Document, Page } from "react-pdf";
 import { useAuthContext } from "../hooks/useAuthContext";
 import { pdfjs } from "react-pdf";
+import { createTw } from "react-pdf-tailwind";
+
+const tw = createTw({
+  theme: {
+    fontFamily: {
+      sans: ["Comic Sans"],
+    },
+    extend: {
+      colors: {
+        custom: "#bada55",
+      },
+    },
+  },
+});
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -39,34 +53,38 @@ const PDFViewer = () => {
     fetchPDF();
   }, [id, user]);
 
-  console.log(pdf);
-  console.log(id);
-  console.log(numPages);
-
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
   };
 
-
   return (
-    <div>
-      <h1>PDF Viewer</h1>
-      <p>
-        Page {pageNumber} of {numPages}
-      </p>
-      <div className="w-96 h-96 bg-black">
-         <Document file={`/uploads/${pdf}`} onLoadSuccess={onDocumentLoadSuccess}>
-        {Array.from(new Array(numPages), (el, index) => (
-          <Page
-            key={`page_${index + 1}`}
-            pageNumber={index + 1}
-            renderTextLayer={false}
-            renderAnnotationLayer={false}
-          />
-        ))}
-      </Document>
+    <div className="flex justify-center items-center min-h-screen bg-gray-100">
+       <h1 className="text-center mb-4">PDF Viewer</h1>
+        <p className="text-center mb-4">
+          Page {pageNumber} of {numPages}
+        </p>
+      <div className="bg-gray-200 p-6 rounded-lg shadow-lg w-full max-w-4xl overflow-y-scroll h-[80vh]">
+       
+        <div className="w-full flex justify-center bg-black mx-auto p-4">
+          <Document
+            file={`/uploads/${pdf}`}
+            onLoadSuccess={onDocumentLoadSuccess}
+            className="space-y-4" // Adds space between the pages
+          >
+            {Array.from(new Array(numPages), (el, index) => (
+              <Page
+                size="A4"
+                style={tw("p-12 font-sans")}
+                key={`page_${index + 1}`}
+                pageNumber={index + 1}
+                renderTextLayer={false}
+                renderAnnotationLayer={false}
+                className="mb-4" // Adds space between the pages
+              />
+            ))}
+          </Document>
+        </div>
       </div>
-     
     </div>
   );
 };
