29eec4732c0b4e4abca32566f19dbea264ff0b10
Kalinga Jayathilaka <kalingajayathilaka@gmail.com>
2024-06-13 21:14:54 +0530

    adding css

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -31,7 +31,8 @@ function Home() {
       setFile(null);
       return;
     }
-    if (selectedFile && selectedFile.size > 30 * 1024 * 1024) { // 30MB
+    if (selectedFile && selectedFile.size > 30 * 1024 * 1024) {
+      // 30MB
       setError("File size must be less than 30MB.");
       setFile(null);
       return;
@@ -61,11 +62,16 @@ function Home() {
 
     if (response.ok) {
       Swal.fire({
-        title: 'Success!',
+        title: "Success!",
         text: `The file "${title}" has been uploaded successfully.`,
-        icon: 'success',
-        confirmButtonText: 'OK'
+        icon: "success",
+        confirmButtonText: "OK",
       });
+
+      // Clear form inputs
+      setTitle("");
+      setFile(null);
+      setError("");
 
       if (user) {
         fetch("/api/pdf", {
@@ -81,13 +87,13 @@ function Home() {
 
   const onDelete = async (id) => {
     Swal.fire({
-      title: 'Are you sure?',
+      title: "Are you sure?",
       text: "You won't be able to revert this!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Yes, delete it!'
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
         fetch(`/api/pdf/${id}`, {
@@ -105,11 +111,7 @@ function Home() {
           }
         });
 
-        Swal.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        );
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
       }
     });
   };
@@ -127,97 +129,134 @@ function Home() {
         >
           <div className="flex items-center justify-between gap-3 mb-2">
             <div className="flex items-center justify-center gap-3">
-            <label
-              htmlFor="title"
-              className="block text-sm font-bold leading-6 text-white"
-            >
-              Title :
-            </label>
-            <div className="mt-2">
-              <input
-                id="title"
-                name="title"
-                type="text"
-                onChange={(e) => setTitle(e.target.value)}
-                value={title}
-                placeholder="File Title"
-                required
-                className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#454545] sm:text-sm sm:leading-6"
-              />
+              <label
+                htmlFor="title"
+                className="block text-sm font-bold leading-6 text-white"
+              >
+                Title :
+              </label>
+              <div className="mt-2">
+                <input
+                  id="title"
+                  name="title"
+                  type="text"
+                  onChange={(e) => setTitle(e.target.value)}
+                  value={title}
+                  placeholder="File Title"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 px-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-[#454545] sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
-          </div>
-          <div className="flex flex-col items-center">
-            <label className="block cursor-pointer">
-              <input
-                type="file"
-                onChange={onFileChange}
-                className="block w-full text-sm cursor-pointer text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 "
-              />
-            </label>
-          </div>
-          <button
-            type="submit"
-            className="flex items-center gap-3 py-2 pl-2 pr-3 text-sm font-medium text-white rounded-full shadow-sm hover:bg-violet-400 group bg-violet-500"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="currentColor"
-              className="size-4"
+            <div className="flex flex-col items-center">
+              <label className="block cursor-pointer">
+                <input
+                  type="file"
+                  onChange={onFileChange}
+                  className="block w-full text-sm cursor-pointer text-slate-500 file:mr-4 file:py-2 file:px-4 file:rounded-full file:border-0 file:text-sm file:font-semibold file:bg-violet-50 file:text-violet-700 hover:file:bg-violet-100 "
+                />
+              </label>
+            </div>
+            <button
+              type="submit"
+              className="flex items-center gap-3 py-2 pl-2 pr-3 text-sm font-medium text-white rounded-full shadow-sm hover:bg-violet-400 group bg-violet-500"
             >
-              <path d="M11.47 1.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06l-1.72-1.72V7.5h-1.5V4.06L9.53 5.78a.75.75 0 0 1-1.06-1.06l3-3ZM11.25 7.5V15a.75.75 0 0 0 1.5 0V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
-            </svg>
-            Upload PDF
-          </button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                className="size-4"
+              >
+                <path d="M11.47 1.72a.75.75 0 0 1 1.06 0l3 3a.75.75 0 0 1-1.06 1.06l-1.72-1.72V7.5h-1.5V4.06L9.53 5.78a.75.75 0 0 1-1.06-1.06l3-3ZM11.25 7.5V15a.75.75 0 0 0 1.5 0V7.5h3.75a3 3 0 0 1 3 3v9a3 3 0 0 1-3 3h-9a3 3 0 0 1-3-3v-9a3 3 0 0 1 3-3h3.75Z" />
+              </svg>
+              Upload PDF
+            </button>
           </div>
-          <div>
-          <span className="mt-2 text-sm text-white">*Only PDF files are allowed. Maximum size: 30MB</span>
-          {error && <div className="mt-2 text-red-500">{error}</div>}
+          <div className="items-center justify-center mx-auto">
+            <span className="mt-2 text-sm text-white">
+              *Only PDF files are allowed. Maximum size: 30MB
+            </span>
+            {error && (
+              <div className="items-center justify-center mx-auto mt-1 text-sm text-red-500">
+                {error}
+              </div>
+            )}
           </div>
         </form>
 
-        <div className="mx-5 my-10 overflow-y-scroll bg-yellow-300">
-          <table className="min-w-full divide-y divide-gray-200">
-            <thead className="bg-gray-50">
-              <tr>
-                <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                  #
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                  File Title
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                  Created Date
-                </th>
-                <th scope="col" className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">
-                  Actions
-                </th>
-              </tr>
-            </thead>
-            <tbody className="bg-white divide-y divide-gray-200">
-              {pdfs.map((pdf, index) => (
-                <tr key={pdf._id}>
-                  <td className="px-6 py-4 whitespace-nowrap">{index + 1}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{pdf.title}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">{new Date(pdf.createdAt).toLocaleDateString()}</td>
-                  <td className="px-6 py-4 whitespace-nowrap">
-                    <Link
-                      to={`/pdf/${pdf._id}`}
-                      className="text-indigo-600 hover:text-indigo-900"
-                    >
-                      View
-                    </Link>
-                    <button
-                      onClick={() => onDelete(pdf._id)}
-                      className="ml-4 text-red-600 hover:text-red-900"
-                    >
-                      Delete
-                    </button>
-                  </td>
+        <div className="mx-5 my-10 max-h-[350px] overflow-hidden bg-yellow-300">
+          <div className="table-wrapper overflow-y-auto max-h-[350px]">
+            <table className="min-w-full divide-y divide-gray-200">
+              <thead className="sticky top-0 bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                  >
+                    #
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                  >
+                    File Title
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                  >
+                    Created Date
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase"
+                  >
+                    Actions
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody className="bg-white divide-y divide-gray-200">
+                {pdfs.length === 0 ? (
+                  <tr>
+                    <td
+                      colSpan="4"
+                      className="px-6 py-4 text-sm text-center text-gray-500"
+                    >
+                      No files found.
+                    </td>
+                  </tr>
+                ) : (
+                  pdfs.map((pdf, index) => (
+                    <tr key={pdf._id}>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {index + 1}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {pdf.title}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        {new Date(pdf.createdAt).toLocaleDateString()}
+                      </td>
+                      <td className="px-6 py-4 whitespace-nowrap">
+                        <Link
+                          to={`/pdf/${pdf._id}`}
+                          className="text-indigo-600 hover:text-indigo-900"
+                        >
+                          View
+                        </Link>
+                        <button
+                          onClick={() => onDelete(pdf._id)}
+                          className="ml-4 text-red-600 hover:text-red-900"
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+                )}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
 
